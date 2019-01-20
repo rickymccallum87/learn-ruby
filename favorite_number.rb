76de1fc151ce7ca334be69c08fc527cd1f062568
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-19 19:20:46 -0500

    Chapter 4 exercises

---
+++ favorite_number.rb
@@ -0,0 +1,3 @@
+puts 'What is your favorite number?'
+fave = gets.chomp.to_i + 1
+puts 'Very nice, but have you considered ' + fave.to_s + '?'
---
+++ greet.rb
@@ -0,0 +1,10 @@
+puts 'First name? '
+first = gets.chomp
+
+puts 'Middle name? '
+middle = gets.chomp
+
+puts 'Last name? '
+last = gets.chomp
+
+puts 'Nice to meet you, ' + first + ' ' + middle + ' ' + last + '!'
