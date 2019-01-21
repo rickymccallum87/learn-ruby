a65dc5d4f8e310bf55a9b868be85bc6073a6d1d1
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-21 10:33:25 -0500

    English number hundreds place

--- english_number.rb
+++ english_number.rb
@@ -1,7 +1,7 @@
 def english_number number
 
     if number < 0
-        return 'Please enter a number that isn\'t negative.'
+        return 'negative'
     end
 
     if number == 0
@@ -14,19 +14,31 @@ def english_number number
     tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
     teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
 
-    # how much of the number remaining to translate?
     remaining = number
-    current = number / 10           # how many tens?
+    current = remaining / 100       # how many hundreds?
+    remaining -= current * 100      # subtract those hundreds
+
+    # speak hundreds
+    if current > 0
+        hundreds = english_number current
+        spoken_number += hundreds + ' hundred'
+
+        if remaining > 0
+            spoken_number += ' '
+        end
+    end
+
+    current = remaining / 10        # how many tens?
     remaining -= current * 10       # subtract those tens
 
     # speak tens
     if current > 0
         # account for teens
         if current == 1 and remaining > 0
-            spoken_number = teens[remaining-1]
+            spoken_number += teens[remaining-1]
             remaining = 0
         else
-            spoken_number = tens[current-1]
+            spoken_number += tens[current-1]
         end
 
         if remaining > 0
@@ -37,6 +49,7 @@ def english_number number
     current = remaining             # how many ones?
     remaining = 0                   # subtract those ones
 
+    # speak ones
     if current > 0
         spoken_number += ones[current-1]
     end
