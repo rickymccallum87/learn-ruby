216669a5b5c160eb496f72f0e0174632383c219e
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-21 09:53:54 -0500

    English number tens place

--- english_number.rb
+++ english_number.rb
@@ -1,23 +1,51 @@
-def english_number num
+def english_number number
 
-    if num < 0
+    if number < 0
         return 'Please enter a number that isn\'t negative.'
     end
 
-    if num == 0
+    if number == 0
         return 'zero'
     end
 
-    str = ''
+    spoken_number = ''
 
     ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
 
-    str = ones[num-1]
+    # how much of the number remaining to translate?
+    remaining = number
+    current = number / 10           # how many tens?
+    remaining -= current * 10       # subtract those tens
 
+    # speak tens
+    if current > 0
+        # account for teens
+        if current == 1 and remaining > 0
+            spoken_number = teens[remaining-1]
+            remaining = 0
+        else
+            spoken_number = tens[current-1]
+        end
+
+        if remaining > 0
+            spoken_number += '-'
+        end
+    end
+
+    current = remaining             # how many ones?
+    remaining = 0                   # subtract those ones
+
+    if current > 0
+        spoken_number += ones[current-1]
+    end
+
+    spoken_number
 end
 
-num = 0
-while num != ''
-    num = gets.chomp
-    puts english_number(num.to_i)
+number = gets.chomp
+while number != ''
+    puts english_number(number.to_i)
+    number = gets.chomp
 end
